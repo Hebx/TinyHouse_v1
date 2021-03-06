fcf3c5c9355ce3bee39e938a1392f9e97bcbeb67
Hebx <lord.noctx@gmail.com>
2021-03-06 05:19:08 +0000

    Querying and Mutating listings data with GraphQL

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,68 +1,13 @@
+import { schema } from "./graphql";
 import express from "express";
-import { Booking, bookings } from "./bookings";
-import { listings } from "./listings";
+import { ApolloServer } from "apollo-server-express";
 
 const app = express();
 const port = 9000;
+const server = new ApolloServer({ schema });
 
-app.use(express.json());
-
-app.get("/listings", (_req, res) => res.send(listings));
-
-app.post("/delete-listings", (req, res) => {
-  const id: string = req.body.id;
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      return res.send(listings.splice(i, 1)[0]);
-    }
-  }
-  return res.send("failed to delete listing");
-});
-
-app.get("/bookings", (_req, res) => {
-  res.send(bookings);
-});
-
-let numOfBookings = 0;
-
-app.post("/create-booking", (req, res) => {
-  const id: string = req.body.id;
-  const timestamp: string = req.body.timestamp;
-
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      numOfBookings++;
-
-      const newBooking: Booking = {
-        id: numOfBookings.toString(),
-        title: listings[i].title,
-        image: listings[i].image,
-        address: listings[i].address,
-        timestamp,
-      };
-
-      bookings.push(newBooking);
-      listings[i].bookings.push(newBooking.id);
-
-      return res.send(newBooking);
-    }
-  }
-  return res.send("failed to create a booking");
-});
-
-app.post("/favorite-listing", (req, res) => {
-  const id: string = req.body.id;
-
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      listings[i].favorite = !listings[i].favorite;
-      return res.send(listings[i]);
-    }
-  }
-  return res.send("failed to favorite a listing");
-});
+server.applyMiddleware({ app, path: "/api" });
 
 app.listen(port);
 
 console.log(`[app] : http://localhost:${port}`);
-console.log(`[timestamp]`, new Date().toLocaleString());
