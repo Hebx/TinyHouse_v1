0098da412004df1a76824a692a7870689a1ed45c
Hebx <lord.noctx@gmail.com>
2021-03-06 12:27:11 +0000

    Connecting our server to MongoDB & Adding types to our Collections

---
+++ server/src/databse/index.ts
@@ -0,0 +1,15 @@
+import { Database } from "./../lib/types";
+import { MongoClient } from "mongodb";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+export const connectDatabase = async (): Promise<Database> => {
+  const client = await MongoClient.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  const db = client.db("main");
+  return {
+    listings: db.collection("listings"),
+  };
+};
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,26 @@
+import dotenv from "dotenv";
+dotenv.config({ path: ".env" });
 import { typeDefs, resolvers } from "./graphql";
-import express from "express";
+import express, { Application } from "express";
 import { ApolloServer } from "apollo-server-express";
+import { connectDatabase } from "./databse";
 
-const app = express();
-const port = 9000;
-const server = new ApolloServer({ typeDefs, resolvers });
+const mount = async (app: Application) => {
+  const db = await connectDatabase();
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: () => ({ db }),
+  });
+  server.applyMiddleware({ app, path: "/api" });
 
-server.applyMiddleware({ app, path: "/api" });
+  app.listen(process.env.PORT);
 
-app.listen(port);
+  const listings = await db.listings.find({}).toArray();
 
-console.log(`[app] : http://localhost:${port}`);
-console.log(`[timestamp]`, new Date().toLocaleString());
+  console.log(`[app] : http://localhost:${process.env.PORT}`);
+  console.log(`[timestamp]`, new Date().toLocaleString());
+  console.log(`[listings]`, listings);
+};
+
+mount(express());
