515b990211fe562cb7b97f03423f9a7007c8ad2e
Hebx <lord.noctx@gmail.com>
2021-03-08 04:56:23 +0000

    Implementing useState hook into our Listings component

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { server } from "../../lib/api";
 import {
   DeleteListingData,
+  Listing,
   DeleteListingVariables,
   ListingsData,
 } from "../../lib/api/types";
@@ -34,27 +35,37 @@ interface Props {
 }
 
 export const Listings = ({ title }: Props) => {
+  const [listings, setListings] = useState<Listing[] | null>(null);
   const fetchListings = async () => {
     const { data } = await server.fetch<ListingsData>({ query: LISTINGS });
-    console.log(data.listings);
+    setListings(data.listings);
   };
-  const deleteListing = async () => {
-    const { data } = await server.fetch<
-      DeleteListingData,
-      DeleteListingVariables
-    >({
+  const deleteListing = async (id: string) => {
+    await server.fetch<DeleteListingData, DeleteListingVariables>({
       query: DELETE_LISTING,
       variables: {
-        id: "6044ad74110393068321a6ef",
+        id,
       },
     });
-    console.log(data);
+    fetchListings();
   };
+  const listingsList = listings ? (
+    <ul>
+      {listings.map((listing) => {
+        return (
+          <li key={listing.id}>
+            {listing.title}{" "}
+            <button onClick={() => deleteListing(listing.id)}>Delete</button>
+          </li>
+        );
+      })}
+    </ul>
+  ) : null;
   return (
     <div>
       <h2> {title} </h2>
+      {listingsList}
       <button onClick={fetchListings}>Query Listing!</button>
-      <button onClick={deleteListing}>Delete a Listing!</button>
     </div>
   );
 };
