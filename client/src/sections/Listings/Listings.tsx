4e8f41ca87f8e3b6a3f99983d566f3f282b5d7d9
Hebx <lord.noctx@gmail.com>
2021-03-21 02:37:23 +0000

    Implementing custom useQuery hook into our Listings component

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,11 +1,10 @@
-import React, { useState, useEffect } from "react";
-import { server } from "../../lib/api";
+import React from "react";
+import { server, useQuery } from "../../lib/api";
 import {
   DeleteListingData,
-  Listing,
   DeleteListingVariables,
   ListingsData,
-} from "../../lib/api/types";
+} from "./types";
 
 const LISTINGS = `
 query Listings {
@@ -35,14 +34,8 @@ interface Props {
 }
 
 export const Listings = ({ title }: Props) => {
-  const [listings, setListings] = useState<Listing[] | null>(null);
-  useEffect(() => {
-    fetchListings();
-  }, []);
-  const fetchListings = async () => {
-    const { data } = await server.fetch<ListingsData>({ query: LISTINGS });
-    setListings(data.listings);
-  };
+  const { data } = useQuery<ListingsData>(LISTINGS);
+
   const deleteListing = async (id: string) => {
     await server.fetch<DeleteListingData, DeleteListingVariables>({
       query: DELETE_LISTING,
@@ -50,8 +43,8 @@ export const Listings = ({ title }: Props) => {
         id,
       },
     });
-    fetchListings();
   };
+  const listings = data ? data.listings : null;
   const listingsList = listings ? (
     <ul>
       {listings.map((listing) => {
